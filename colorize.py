53db83f6711579ca144d2e95c5bab1c8d7148b3c
Kristjan-Julius Laak <julius.laak@gmail.com>
2014-12-10 19:57:50 +0200

    RGB2YIQ conversion
    fails atm

--- colorize.py
+++ colorize.py
@@ -1,7 +1,12 @@
 from scipy import misc
 import numpy as np
 import matplotlib.pyplot as plt
+import colorsys
 import os
+
+def my_rgb2ntsc(rgb):
+    yiq = rgb
+    return yiq
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -11,7 +16,19 @@ marked = misc.imread(os.path.join(dir_path, 'example_marked.bmp'))
 original = original.astype(float)/255
 marked = marked.astype(float)/255
 
+# values on True/False not 1/0 as in MATLAB - deal later with it!
+colorIm = abs(original - marked).sum(2) > 0.01
+
+(Y,I,Q) = colorsys.rgb_to_yiq(original.take((0,),2),original.take((1,),2),original.take((2,),2))
+
+yiq_gray = np.zeros(original.shape)
+yiq_gray[:][:][0] = Y
+yiq_gray[:][:][1] = I
+yiq_gray[:][:][2] = Q
+
 """
+YIQ_gray = my_rgb2ntsc(original);
+YIQ_color = my_rgb2ntsc(marked);
 print 'image'
 print image
 print 'shape'
