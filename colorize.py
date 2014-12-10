a4059c173e3ce769c954f49b5e4e36b32cfa3699
geap <geapajula@gmail.com>
2014-12-10 14:08:08 +0200

    loading, displaying image

--- colorize.py
+++ colorize.py
@@ -6,12 +6,12 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 image_name = 'example.bmp'
 
+# http://samarthbhargav.wordpress.com/2014/05/05/image-processing-with-python-rgb-to-grayscale-conversion/
+
 image = misc.imread(os.path.join(dir_path, image_name))
 print image
 print image.shape
 print image[0][0]
-
-# http://samarthbhargav.wordpress.com/2014/05/05/image-processing-with-python-rgb-to-grayscale-conversion/
 
 plt.imshow(image) #load
 plt.show()  # show the window
