0200bc3c06ceccbc84233d324e6ffaf7ad0cf880
Kristjan-Julius Laak <julius.laak@gmail.com>
2014-12-10 19:05:38 +0200

    Python cg

--- colorize.py
+++ colorize.py
@@ -9,14 +9,18 @@ image_name = 'example.bmp'
 # http://samarthbhargav.wordpress.com/2014/05/05/image-processing-with-python-rgb-to-grayscale-conversion/
 
 image = misc.imread(os.path.join(dir_path, image_name))
+
+"""
+print 'image'
 print image
+print 'shape'
 print image.shape
+print 'esimese pixli rgb'
 print image[0][0]
 
 plt.imshow(image) #load
 plt.show()  # show the window
-
-#...
+"""
 
 image_name_2 = 'example_copy.bmp'
 misc.imsave(os.path.join(dir_path, image_name_2), image)
