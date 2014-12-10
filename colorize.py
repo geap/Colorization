f99b24701295eca92040170cc4de7e2eb1a8338b
geap <geapajula@gmail.com>
2014-12-10 19:04:47 +0200

    testing merge

--- colorize.py
+++ colorize.py
@@ -6,8 +6,6 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 image_name = 'example.bmp'
 
-# http://samarthbhargav.wordpress.com/2014/05/05/image-processing-with-python-rgb-to-grayscale-conversion/
-
 image = misc.imread(os.path.join(dir_path, image_name))
 print image
 print image.shape
@@ -15,8 +13,6 @@ print image[0][0]
 
 plt.imshow(image) #load
 plt.show()  # show the window
-
-#...
 
 image_name_2 = 'example_copy.bmp'
 misc.imsave(os.path.join(dir_path, image_name_2), image)
