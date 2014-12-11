89bcc127eff8067588ee1466bfa2d3d8bf27badb
geap <geapajula@gmail.com>
2014-12-11 02:05:17 +0200

    image loading

--- colorize.py
+++ colorize.py
@@ -7,18 +7,12 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 image_name = 'example.bmp'
 
 image = misc.imread(os.path.join(dir_path, image_name))
-
-"""
-print 'image'
 print image
-print 'shape'
 print image.shape
-print 'esimese pixli rgb'
 print image[0][0]
 
 plt.imshow(image) #load
 plt.show()  # show the window
-"""
 
 image_name_2 = 'example_copy.bmp'
 misc.imsave(os.path.join(dir_path, image_name_2), image)
