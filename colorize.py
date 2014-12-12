73ae65e23c44278023837215ba044f3321dff4a2
Kristjan-Julius Laak <julius.laak@gmail.com>
2014-12-12 03:51:20 +0200

    Main part done

--- colorize.py
+++ colorize.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 import colorsys
 import os
 
+def getColorExact( colorIm, YUV):
+    # THIS IS A MOCK F-N
+    return YUV
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 original = misc.imread(os.path.join(dir_path, 'example.bmp'))
@@ -15,22 +19,30 @@ marked = marked.astype(float)/255
 # values on True/False not 1/0 as in MATLAB - deal later with it!
 colorIm = abs(original - marked).sum(2) > 0.01
 
-(Y,I,Q) = colorsys.rgb_to_yiq(original[:,:,0],original[:,:,1],original[:,:,2])
-(Y2,I2,Q2) = colorsys.rgb_to_yiq(marked[:,:,0],marked[:,:,1],marked[:,:,2])
+(Y,_,_) = colorsys.rgb_to_yiq(original[:,:,0],original[:,:,1],original[:,:,2])
+(_,I,Q) = colorsys.rgb_to_yiq(marked[:,:,0],marked[:,:,1],marked[:,:,2])
 
-yiq_gray = np.zeros(original.shape)
-yiq_color = np.zeros(marked.shape)
-yiq_gray[:,:,0] = Y
-yiq_gray[:,:,1] = I
-yiq_gray[:,:,2] = Q
-yiq_color[:,:,0] = Y2
-yiq_color[:,:,1] = I2
-yiq_color[:,:,2] = Q2
+# YUV aka ntscIm
+YUV = np.zeros(original.shape)
+YUV[:,:,0] = Y
+YUV[:,:,1] = I
+YUV[:,:,2] = Q
 
-"""
-plt.imshow(original) #load
-plt.show()  # show the window
-"""
+max_d = np.floor(np.log(min(YUV.shape[0],YUV.shape[1]))/np.log(2)-2)
 
-#image_name_2 = 'example_copy.bmp'
-#misc.imsave(os.path.join(dir_path, image_name_2), image)
+iu = np.floor(YUV.shape[0]/(2**(max_d - 1))) * (2**(max_d - 1))
+ju = np.floor(YUV.shape[1]/(2**(max_d - 1))) * (2**(max_d - 1))
+
+print colorIm.shape
+colorIm = colorIm[:iu,:ju]
+YUV = YUV[:iu,:ju]
+
+
+# SOLVE THIS PROBLEM
+colorized = abs(getColorExact( colorIm, YUV ));
+
+
+plt.imshow(colorized)
+plt.show()
+
+#misc.imsave(os.path.join(dir_path, 'example_colorized.bmp'), image)
