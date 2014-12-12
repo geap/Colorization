faa440cde5b1307de0f6423366bd97cdaf65ea48
Kristjan-Julius Laak <julius.laak@gmail.com>
2014-12-12 03:20:09 +0200

    rgb2ntsc implementation works

--- colorize.py
+++ colorize.py
@@ -15,21 +15,20 @@ marked = marked.astype(float)/255
 # values on True/False not 1/0 as in MATLAB - deal later with it!
 colorIm = abs(original - marked).sum(2) > 0.01
 
-(Y,I,Q) = colorsys.rgb_to_yiq(original.take((0,),2),original.take((1,),2),original.take((2,),2))
+(Y,I,Q) = colorsys.rgb_to_yiq(original[:,:,0],original[:,:,1],original[:,:,2])
+(Y2,I2,Q2) = colorsys.rgb_to_yiq(marked[:,:,0],marked[:,:,1],marked[:,:,2])
 
 yiq_gray = np.zeros(original.shape)
-yiq_gray[:][:][0] = Y
-yiq_gray[:][:][1] = I
-yiq_gray[:][:][2] = Q
+yiq_color = np.zeros(marked.shape)
+yiq_gray[:,:,0] = Y
+yiq_gray[:,:,1] = I
+yiq_gray[:,:,2] = Q
+yiq_color[:,:,0] = Y2
+yiq_color[:,:,1] = I2
+yiq_color[:,:,2] = Q2
 
 """
-YIQ_gray = my_rgb2ntsc(original);
-YIQ_color = my_rgb2ntsc(marked);
-print 'image'
-print image
-print image.shape
-print image[0][0]
-plt.imshow(image) #load
+plt.imshow(original) #load
 plt.show()  # show the window
 """
 
