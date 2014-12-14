b5cfe2fdfff6d8b250637d594b27435617ac5f59
geap <geapajula@gmail.com>
2014-12-14 03:26:39 +0200

    working on sparse and Ax=b solving

--- colorize.py
+++ colorize.py
@@ -5,6 +5,9 @@ import colorsys
 import os
 import time
 import sys
+import scipy.sparse as sps
+from scipy.sparse.linalg import spsolve
+from scipy import io
 
 os.system('cls')
 os.system('reset')
@@ -165,10 +168,203 @@ YUV = YUV[:iu,:ju]
 '''
 
 # SOLVE THIS PROBLEM
-colorized = abs(getColorExact( colorIm, YUV ));
+#colorized = abs(getColorExact( colorIm, YUV ));
+
+############## getColorExact Start ################
+#def getColorExact( colorIm, YUV):
+# YUV as ntscIm
+
+n = YUV.shape[0]
+m = YUV.shape[1]
+
+image_size = n*m
+
+# colorized as nI
+colorized = np.zeros(YUV.shape)
+# luma component stays the same
+colorized[:,:,0] = YUV[:,:,0]
+
+
+# this Matlab code: z = reshape(x,3,4); should become z = x.reshape(3,4,order='F').copy() in Numpy.
+# indices_matrix as indsM
+indices_matrix = np.arange(image_size).reshape(n,m,order='F').copy()
+
+# We have to reshape and make a copy of the view of an array
+# for the nonzero() work like in MATLAB
+color_copy_for_nonzero = colorIm.reshape(image_size).copy()
+
+# label_inds as lblInds
+#label_inds = np.count_nonzero(color_copy_for_nonzero) # it's cool that nonzero likes boolean values, too
+# the indeces not the count of them is needed
+label_inds = np.nonzero(color_copy_for_nonzero) # it's cool that nonzero likes boolean values, too
+
+wd = 1
+
+# length as len (for obv reasons)
+length = 0
+col_inds = np.zeros((image_size*( 2 * wd + 1 )**2,1), dtype=np.int64)
+row_inds = np.zeros((image_size*( 2 * wd + 1 )**2,1), dtype=np.int64)
+vals = np.zeros((image_size*( 2 * wd + 1 )**2,1))
+gvals = np.zeros((2 * wd + 1 )**2)
+
+
+# PREPS made, lets ITERATE!
+count = 0 # for testing
+consts_len = 0
+for j in range(m):
+    for i in range(n):
+        consts_len += 1
+        
+        if (not colorIm[i,j]):
+            tlen = 0
+            
+            #print max( 0, i - wd ), min( i + wd+1, n ),max( 0, j - wd ), min( j + wd, m )+1
+            for ii in range(max( 0, i-wd ), min( i+wd+1, n )):
+                for jj in range( max( 0, j - wd ), min( j + wd, m )+1):
+                    count += 1 # for testing
+                    if ( ii != i or jj != j ):
+                        row_inds[length,0] = consts_len
+                        col_inds[length,0] = indices_matrix[ii,jj]
+                        gvals[tlen] = YUV[ii,jj,0]
+                        length += 1
+                        tlen += 1
+            
+            t_val = YUV[i,j,0].copy()
+            gvals[tlen] = t_val
+            c_var = np.mean((gvals[0:tlen+1] - np.mean(gvals[0:tlen+1]))**2)
+            csig = c_var * 0.6
+            mgv = min(( gvals[0:tlen+1] - t_val )**2)
+            
+            if (csig < ( -mgv / np.log(0.01 ))):
+                csig = -mgv / np.log(0.01)
+            if (csig <0.000002):
+                csig = 0.000002
+            
+            gvals[0:tlen] = np.exp( -(gvals[0:tlen] - t_val)**2 / csig )
+            gvals[0:tlen] = gvals[0:tlen] / np.sum(gvals[0:tlen])
+            vals[length-tlen:length,0] = -gvals[0:tlen]
+        
+        # END IF
+        
+        length += 1
+        row_inds[length-1,0] = consts_len
+        col_inds[length-1,0] = indices_matrix[i,j]
+        vals[length-1,0] = 1
+        
+        
+
+    # END OF FOR i
+# END OF FOR j
+
+
+# A LITTLE BIT MORE AND THEN CAN RETURN ALREADY SOMETHING!
+
+vals = vals[0:length,0]
+col_inds = col_inds[0:length,0]
+row_inds = row_inds[0:length,0]
+
+# decrease indexes by 1 because somehow sparse increases indexes by 1
+#col_inds[col_inds > 0] = col_inds[col_inds > 0] - 1
+#row_inds[row_inds > 0] = row_inds[row_inds > 0] - 1
+
+# otherwise index out of bounds
+if (row_inds[row_inds.shape[0]-1] == consts_len):
+    row_inds[row_inds.shape[0]-1] = consts_len - 1
+
+# A=sparse(row_inds,col_inds,vals,consts_len,imgSize);
+    
+'''
+S = sparse(i,j,s,m,n,nzmax) uses vectors i, j, and s to generate an
+m-by-n sparse matrix such that S(i(k),j(k)) = s(k), with space
+allocated for nzmax nonzeros.  Vectors i, j, and s are all the same
+length.  Any elements of s that are zero are ignored, along with the
+corresponding values of i and j.  Any elements of s that have duplicate
+values of i and j are added together.  The argument s and one of the
+arguments i or j may be scalars, in which case the scalars are expanded
+so that the first three arguments all have the same length.
+
+>> a = diag(1:4)
+
+a =
+
+    1     0     0     0
+    0     2     0     0
+    0     0     3     0
+    0     0     0     4
+
+>> s = sparse(a)
+
+s =
+
+(1,1)        1
+(2,2)        2
+(3,3)        3
+(4,4)        4
+'''
+
+
+# csr_matrix((data, ij), [shape=(M, N)])
+# where data and ij satisfy the relationship a[ij[0, k], ij[1, k]] = data[k]
+
+# make the sparse matrix
+A = sps.coo_matrix((vals, (row_inds, col_inds)), (consts_len, image_size))
+#A_dense = A.todense() # MemoryError
+# write to a file instead
+io.mmwrite(os.path.join(dir_path, 'sparse_matrix'), A)
+# for some unknown reason all the coordinates are increased by 1
+print "row_inds"
+print row_inds[0:10]
+print "col_inds"
+print col_inds[0:10]
+print "vals"
+print vals[0:10]
+
+# The CSR format is specially suitable for fast matrix vector product
+# Try first with the same format as in the example in http://docs.scipy.org/doc/scipy-0.14.0/reference/sparse.html
+A = A.tolil().tocsr()
+#io.mmwrite(os.path.join(dir_path, 'sparse_matrix_lil'), A)
+
+print 'Get the sparse matrix correct and then continue :)'
+
+
+#b = np.zeros((A.shape[0], 1))
+#curIm = np.zeros(YUV.shape)
+for t in [1,2]: # not [2,3] due to different indexing 
+    print t
+    curIm = YUV[:,:,t]
+    curIm = curIm.reshape(image_size, 1)
+    b = np.zeros((image_size, 1))
+    b[label_inds] = curIm[label_inds]
+    #new_vals = np.divide(A, b) # very, very slow
+    new_vals = spsolve(A, b) # this should work, see examples from http://docs.scipy.org/doc/scipy-0.14.0/reference/sparse.html
+    # sps.linalg.inv(A) # A is singular, there is no inverse matrix
+    #new_vals = sps.linalg.lsqr(A, b) # this works endlessly...
+    print "new vals"
+    print new_vals
+    colorized[:,:,t] = new_vals.reshape(n, m, order='F').copy()
+
+#snI = colorized
+
+#(R, G, B) = colorsys.yiq_to_rgb(colorized[:,:,0],colorized[:,:,1],colorized[:,:,2])
+
+#colorizedIm = np.zeros(original.shape)
+#colorizedIm[:,:,0] = R
+#colorizedIm[:,:,1] = G
+#colorizedIm[:,:,2] = B
+
+
+#print something
+#sys.exit('Sparse needs to be implemented!')
+
+#scipy.sparse.csr_matrix
+
+    
+#    return YUV # should be colorized, but mock until we make it
+
+################## getColorExact end ##############
 
 
 #plt.imshow(colorized)
 #plt.show()
 
-#misc.imsave(os.path.join(dir_path, 'example_colorized.bmp'), image)
+#misc.imsave(os.path.join(dir_path, 'example_colorized.bmp'), colorizedIm)
